40972eff105c7626c0946d5db2396718c908901d
Emilse Arias <emilsearias@gmail.com>
2025-08-21 20:22:56 -0300

    agregamos el maquetado de una card con el personaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Personaje from "./components/Personaje"
+import logo from './assets/Dragon_Ball_Z_logo.svg'
+import { Button } from "react-bootstrap"
 
 function App() {
  
 
   return (
-    <>
-     <h1>hola mundo</h1>
+    <main className="container my-5 text-center">
+     <img src={logo} alt="logo de dragon ball z" className="w-50 mb-4" /> 
      <Personaje></Personaje>
-    </>
+     <Button variant="danger" className="mt-4">Obtener personaje</Button>
+    </main>
   )
 }
 
--- src/components/Personaje.jsx
+++ src/components/Personaje.jsx
@@ -1,16 +1,21 @@
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 const Personaje = () => {
   return (
     <Card>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Header className="fw-bold fs-3">Goku</Card.Header>
+      <Card.Img
+        variant="top"
+        src="https://dragonball-api.com/characters/goku_normal.webp"
+        className="img-card"
+      />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>Caracteristicas</Card.Title>
+        <ul className="list-group">
+          <li className="list-group-item">Raza: Saiyan</li>
+          <li className="list-group-item">ki: 60.000.000</li>
+          <li className="list-group-item">Grupo: Z Fighter</li>
+        </ul>
       </Card.Body>
     </Card>
   );
