4ec0a61382fdc3d2394168581b977fac77d063ce
Emilse Arias <emilsearias@gmail.com>
2025-08-21 21:12:45 -0300

    agregamos la logica para pedir la frase del personaje goku

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import Personaje from "./components/Personaje"
-import logo from './assets/Dragon_Ball_Z_logo.svg'
-import { Button } from "react-bootstrap"
+import Personaje from "./components/Personaje";
+import logo from "./assets/Dragon_Ball_Z_logo.svg";
+import { Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 function App() {
- 
+  const [datoPersonaje, setDatoPersonaje] = useState({});
+
+  useEffect(() => {
+    consultaAPI();
+  }, []);
+
+  const consultaAPI = async() => {
+    try {
+      //aqui van todas las lineas de codigo que quiero
+      const respuesta = await fetch('https://dragonball-api.com/api/characters/1')
+      console.log(respuesta)
+      if(respuesta.status === 200){
+        const datos = await respuesta.json()
+        console.log(datos)
+        setDatoPersonaje(datos)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  };
 
   return (
     <main className="container my-5 text-center">
-     <img src={logo} alt="logo de dragon ball z" className="w-50 mb-4" /> 
-     <Personaje></Personaje>
-     <Button variant="danger" className="mt-4">Obtener personaje</Button>
+      <img src={logo} alt="logo de dragon ball z" className="w-50 mb-4" />
+      <Personaje></Personaje>
+      <Button variant="danger" className="mt-4">
+        Obtener personaje
+      </Button>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
